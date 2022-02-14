62bd787a44a5cc1906a2e2b2fd80be4eea9822d5
wellsky <wellsky@mail.ru>
2022-02-14 15:33:38 +0300

    Image compressor lib added and tested

--- app/src/main/java/club/electro/MainViewModel.kt
+++ app/src/main/java/club/electro/MainViewModel.kt
@@ -1,5 +1,7 @@
 package club.electro
 
+import android.content.Context
+import android.graphics.Bitmap
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,6 +10,9 @@ import club.electro.api.ApiService
 import club.electro.auth.AppAuth
 import club.electro.model.NetworkStatus
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import id.zelory.compressor.Compressor
+import id.zelory.compressor.constraint.*
 import kotlinx.coroutines.launch
 import okhttp3.MediaType
 import okhttp3.MediaType.Companion.toMediaType
@@ -22,6 +27,7 @@ import java.io.File
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
+    @ApplicationContext val context: Context,
     val appAuth: AppAuth,
     val networkStatus : NetworkStatus,
     val apiService: ApiService,
@@ -33,12 +39,25 @@ class MainViewModel @Inject constructor(
 
     init {
         viewModelScope.launch {
-            val file = File("/storage/emulated/0/DCIM/Camera/IMG_20220213_130617.jpg")
+            //val sourceFile = File("/storage/emulated/0/DCIM/Camera/IMG_20220213_130617.jpg")
+            val sourceFile = File("/storage/emulated/0/Download/86262-gorodskoj_rajon-gorodskoj_pejzazh-otrazhenie-gorizont-boston-3840x2160.jpg")
+
+            println("Source file name " + sourceFile.name)
+
+            val outputDir = context.cacheDir // context being the Activity pointer
+            val destinationFile = File.createTempFile("temp_compressed", ".jpg", outputDir)
+            destinationFile.deleteOnExit()
+
+            val compressedImageFile = Compressor.compress(context, sourceFile) {
+                destination(destinationFile )
+                default(width = 1920, format = Bitmap.CompressFormat.JPEG, quality = 80)
+            }
+
             apiService.uploadPostDraftAttachment(
                 file = MultipartBody.Part.createFormData(
                         "file",
-                        file.name,
-                        file.asRequestBody("image/*".toMediaType())
+                        compressedImageFile.name,
+                        compressedImageFile.asRequestBody("image/*".toMediaType())
                 )
             )
         }
