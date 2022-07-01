8fd0f74819b8daebdce7e0d30db2b71b3ae1f5bd
wellsky <wellsky@mail.ru>
2022-07-01 22:35:31 +0300

    Add notifications texts + authors avatars + refactoring

--- app/src/main/java/club/electro/service/FCMService.kt
+++ app/src/main/java/club/electro/service/FCMService.kt
@@ -1,15 +1,19 @@
 package club.electro.service
 
 import android.app.Notification
+import android.app.Notification.Builder.recoverBuilder
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.app.Person
+import androidx.core.graphics.drawable.IconCompat
 import androidx.navigation.NavDeepLinkBuilder
 import club.electro.MainActivity
 import club.electro.R
@@ -17,21 +21,27 @@ import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.google.gson.Gson
 import club.electro.auth.AppAuth
-import club.electro.dao.NotificationDao
-import club.electro.entity.NotificationEntity
+import club.electro.dto.ThreadType
 import club.electro.repository.notifications.NotificationsRepository
 import club.electro.ui.thread.ThreadFragment.Companion.targetPostId
 import club.electro.ui.thread.ThreadFragment.Companion.threadId
 import club.electro.ui.thread.ThreadFragment.Companion.threadType
 import club.electro.utils.GetCircleBitmap
 import club.electro.utils.toPlainText
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.target.Target
+import com.bumptech.glide.request.transition.Transition
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.withContext
 import java.io.IOException
 import java.net.URL
@@ -53,6 +63,9 @@ class FCMService : FirebaseMessagingService() {
     private val contentKey = "content"
     private val channelIdKey = "remote"
 
+    private val groupKey = "electro_club" // Нотификашки группируются все в одну по этому ключу
+    private val groupTitle = "electro.club" // Заголовок суммарной нотификашки
+
     private val gson = Gson()
 
     @Inject
@@ -62,7 +75,8 @@ class FCMService : FirebaseMessagingService() {
     lateinit var notificationsRepository: NotificationsRepository
 
     @Inject
-    @ApplicationContext lateinit var context: Context
+    @ApplicationContext
+    lateinit var context: Context
 
     override fun onCreate() {
         super.onCreate()
@@ -83,18 +97,20 @@ class FCMService : FirebaseMessagingService() {
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
-        val recipient = message.data[recipientIdKey]
-        val currentUserId = appAuth.myId()
+        runBlocking {
+            val recipient = message.data[recipientIdKey]
+            val currentUserId = appAuth.myId()
 
-        recipient?.let {
-            if (it.toLong() == currentUserId) {
-                try {
-                    handleNewMessage(message)
-                } catch (e: Exception) {
-                    println("Error: " + e.message.toString())
+            recipient?.let {
+                if (it.toLong() == currentUserId) {
+                    try {
+                        handleNewMessage(message)
+                    } catch (e: Exception) {
+                        println("Error: " + e.message.toString())
+                    }
+                } else {
+                    appAuth.sendPushToken()
                 }
-            } else {
-                appAuth.sendPushToken()
             }
         }
     }
@@ -105,7 +121,13 @@ class FCMService : FirebaseMessagingService() {
     }
 
     // https://medium.com/@sidorovroman3/android-how-to-use-messagingstyle-for-notifications-without-caching-messages-c414ef2b816c
-    private fun addReply(message: CharSequence, notificationId: Int) {
+    private suspend fun addReplyToNotification(
+        notificationId: Int,
+        postNotification: PostNotification,
+    ) {
+        val text = postNotification.postContent.toPlainText()
+        val userName = postNotification.authorName
+
         // Find notification that you want to update.
         val activeNotification = findActiveNotification(notificationId) ?: return
 
@@ -113,124 +135,130 @@ class FCMService : FirebaseMessagingService() {
         val activeStyle = NotificationCompat.MessagingStyle.extractMessagingStyleFromNotification(activeNotification)
 
         // Recover builder from the active notification.
-        val recoveredBuilder = Notification.Builder.recoverBuilder(context, activeNotification)
+        val recoveredBuilder = NotificationCompat.Builder(context, activeNotification)
+
+        val thread: Person = Person
+            .Builder()
+            .setName(postNotification.threadName)
+            //.setIcon(IconCompat.createWithContentUri(postNotification.threadImage))
+            .build()
 
         // The recoveredBuilder is Notification.Builder whereas the activeStyle is NotificationCompat.MessagingStyle.
         // It means you need to recreate the style as Notification.MessagingStyle to make it compatible with the builder.
-        val newStyle = Notification.MessagingStyle("you")
+        val newStyle = NotificationCompat.MessagingStyle(thread)
         newStyle.conversationTitle = activeStyle?.conversationTitle
         activeStyle?.messages?.forEach {
-            newStyle.addMessage(Notification.MessagingStyle.Message(it.text, it.timestamp, it.sender))
+            newStyle.addMessage(NotificationCompat.MessagingStyle.Message(it.text, it.timestamp, it.person))
         }
+
+        //val authorIcon = loadUrl(postNotification.authorName)
+
+        val author: Person = Person
+            .Builder()
+            .setName(postNotification.authorName)
+            .setIcon(IconCompat.createWithBitmap(loadUrl(postNotification.authorImage)))
+            .build()
+
         // Add your reply to the new style.
-        newStyle.addMessage(Notification.MessagingStyle.Message(message, System.currentTimeMillis(), "you"))
+        newStyle.addMessage(NotificationCompat.MessagingStyle.Message(text, System.currentTimeMillis(), author))
 
         // Set the new style to the recovered builder.
         recoveredBuilder.setStyle(newStyle)
+
+        // TODO удалить когда будет реализована подгрузка иконок через setIcon()
+        // applyImageUrl(recoveredBuilder, postNotification.threadImage)
 
         // Update the active notification.
         NotificationManagerCompat.from(context).notify(notificationId, recoveredBuilder.build())
     }
 
-    private fun createMessagingStyle(message: RemoteMessage): NotificationCompat.MessagingStyle {
-        val data = gson.fromJson(message.data[contentKey], PostNotification::class.java)
-
-        val person: Person = Person.Builder().setName(data.authorName).build()
-
-        return NotificationCompat.MessagingStyle(person)
-            .setConversationTitle("Team lunch")
-            .addMessage("Hi", data.published, person)
+    private fun defaultNotificationBuilder(): NotificationCompat.Builder {
+        return NotificationCompat.Builder(this, channelIdKey)
+            .setSmallIcon(R.drawable.electro_club_icon_grey_64)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setGroup(groupKey)
     }
 
-    private fun handleNewMessage(message: RemoteMessage) {
+    private fun getExistingConversation(
+        threadType: Byte,
+        threadId: Long,
+    ): Int? {
+        val existsId = notificationsRepository.getByThread(threadType, threadId)
+
+        if (existsId != null) {
+            if (findActiveNotification(existsId) != null) {
+                return existsId
+            }
+        }
+        return null
+    }
+
+    private fun NotificationCompat.Builder.createConversation(
+        data: PostNotification
+    ): NotificationCompat.Builder {
+        // https://stackoverflow.com/questions/26608627/how-to-open-fragment-page-when-pressed-a-notification-in-android
+        val resultPendingIntent = NavDeepLinkBuilder(context)
+            .setComponentName(MainActivity::class.java)
+            .setGraph(R.navigation.mobile_navigation)
+            .setDestination(R.id.threadFragment)
+            .setArguments(Bundle().apply {
+                threadType = data.threadType
+                threadId = data.threadId
+                targetPostId = data.postId
+            })
+            .createPendingIntent()
+
+        this.setContentIntent(resultPendingIntent)
+
+
+        val thread: Person = Person
+            .Builder()
+            .setName(data.threadName)
+            .setIcon(IconCompat.createWithBitmap(loadUrl(data.threadImage)))
+            .build()
+
+        val author: Person = Person
+            .Builder()
+            .setName(data.authorName)
+            .setIcon(IconCompat.createWithBitmap(loadUrl(data.authorImage)))
+            .build()
+
+        val style = NotificationCompat.MessagingStyle(thread)
+            .setConversationTitle(data.threadName)
+            .addMessage(data.postContent.toPlainText(), data.published, author)
+
+        if (data.threadType != ThreadType.THREAD_TYPE_PERSONAL_CHAT.value) {
+            style.isGroupConversation = true
+        }
+
+        this.setStyle(style)
+
+        //applyImageUrl(this, data.threadImage)
+
+        return this
+    }
+
+    private suspend fun handleNewMessage(message: RemoteMessage) {
         val action = message.data[actionKey]
 
         when (action) {
             ACTION_PERSONAL_MESSAGE, ACTION_THREAD_POST, ACTION_ANSWER, ACTION_MENTION, ACTION_QUOTE -> {
-                val data = gson.fromJson(message.data[contentKey], PostNotification::class.java)
-                val groupTitle = "new messages"
-                val groupKey = "electro.club" // Нотификашки группируются все в одну по этому ключу
+                val postNotification = gson.fromJson(message.data[contentKey], PostNotification::class.java)
 
-                // https://stackoverflow.com/questions/26608627/how-to-open-fragment-page-when-pressed-a-notification-in-android
-                val resultPendingIntent = NavDeepLinkBuilder(this)
-                    .setComponentName(MainActivity::class.java)
-                    .setGraph(R.navigation.mobile_navigation)
-                    .setDestination(R.id.threadFragment)
-                    .setArguments(Bundle().apply {
-                        threadType = data.threadType
-                        threadId = data.threadId
-                        targetPostId = data.postId
-                    })
-                    .createPendingIntent()
+                val notificationBuilder = defaultNotificationBuilder()
 
-                val notificationBuilder = NotificationCompat.Builder(this, channelIdKey)
-                    .setSmallIcon(R.drawable.electro_club_icon_grey_64)
-                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                    .setContentIntent(resultPendingIntent)
-                    .setGroup(groupKey)
-
-                if (action == ACTION_PERSONAL_MESSAGE) {
-                    val existsId = notificationsRepository.getByThread(data.threadType, data.threadId)
-                    println("EXISTS")
-                    println(existsId)
-                    if (existsId != null) {
-                        if (findActiveNotification(existsId) != null) {
-                            addReply(data.postContent, existsId)
-                            return
-                        } else {
-                            notificationsRepository.clearByThread(data.threadType, data.threadId)
-                        }
-                    }
-
-                    notificationBuilder
-                        .setStyle(createMessagingStyle(message))
-                        .build()
+                getExistingConversation(postNotification.threadType, postNotification.threadId)?.let {
+                    addReplyToNotification(
+                        notificationId = it,
+                        postNotification = postNotification,
+                    )
+                    return@handleNewMessage
                 }
 
-                if (action == ACTION_THREAD_POST) {
-                    val text = data.authorName + ": " + data.postContent.toPlainText()
-                    notificationBuilder
-                        .setContentTitle(data.threadName.toPlainText())
-                        .setContentText(text)
-                        .setStyle(NotificationCompat.BigTextStyle()
-                            .bigText(text)
-                        )
-                }
+                notificationsRepository.clearByThread(postNotification.threadType, postNotification.threadId)
 
-                if (action == ACTION_ANSWER) {
-                    val text = getString(R.string.notification_answer, data.authorName) + ": " + data.postContent.toPlainText()
-                    notificationBuilder
-                        .setContentTitle(data.threadName.toPlainText())
-                        .setContentText(text)
-                        .setStyle(NotificationCompat.BigTextStyle()
-                            .bigText(text)
-                        )
-                }
+                notificationBuilder.createConversation(postNotification)
 
-                if (action == ACTION_QUOTE) {
-                    val text = getString(R.string.notification_quoted, data.authorName) + ": " + data.postContent.toPlainText()
-                    notificationBuilder
-                        .setContentTitle(data.threadName.toPlainText())
-                        .setContentText(text)
-                        .setStyle(NotificationCompat.BigTextStyle()
-                            .bigText(text)
-                        )
-                }
-
-                if (action == ACTION_MENTION) {
-                    val text = getString(R.string.notification_mention, data.authorName) + ": " + data.postContent.toPlainText()
-                    notificationBuilder
-                        .setContentTitle(data.threadName.toPlainText())
-                        .setContentText(text)
-                        .setStyle(NotificationCompat.BigTextStyle()
-                            .bigText(text)
-                        )
-                }
-
-
-
-
-                applyImageUrl(notificationBuilder, data.threadImage)
                 val notification = notificationBuilder.build()
 
                 val notificationId = Random.nextInt(100_000)
@@ -240,24 +268,28 @@ class FCMService : FirebaseMessagingService() {
 
                 notificationsRepository.insert(
                     notificationId = notificationId,
-                    threadType = data.threadType,
-                    threadId = data.threadId
+                    threadType = postNotification.threadType,
+                    threadId = postNotification.threadId
                 )
 
-                // Нотификашка, которая помещает в себя все остальные нотификашки по ключу groupKey
-                val summaryNotification = NotificationCompat.Builder(this, channelIdKey)
-                    .setSilent(true)
-                    .setContentText(groupTitle)
-                    .setSubText(groupTitle)
-                    .setSmallIcon(R.drawable.electro_club_icon_grey_64)
-                    .setGroup(groupKey)
-                    .setGroupSummary(true)
-                    .build()
-
-                NotificationManagerCompat.from(this)
-                    .notify(1, summaryNotification)
+                addGroupNotification()
             }
         }
+    }
+
+    private fun addGroupNotification() {
+        // Нотификашка, которая помещает в себя все остальные нотификашки по ключу groupKey
+        val summaryNotification = NotificationCompat.Builder(this, channelIdKey)
+            .setSilent(true)
+            .setContentText(groupTitle)
+            .setSubText(groupTitle)
+            .setSmallIcon(R.drawable.electro_club_icon_grey_64)
+            .setGroup(groupKey)
+            .setGroupSummary(true)
+            .build()
+
+        NotificationManagerCompat.from(this)
+            .notify(1, summaryNotification)
     }
 
     private fun applyImageUrl(
@@ -280,11 +312,50 @@ class FCMService : FirebaseMessagingService() {
     }
 }
 
-data class PostNotification (
+private fun loadUrl(
+    imageUrl: String
+): Bitmap? = runBlocking {
+    val url = URL(imageUrl)
+
+    withContext(Dispatchers.IO) {
+        try {
+            val input = url.openStream()
+            BitmapFactory.decodeStream(input)
+        } catch (e: IOException) {
+            null
+        }
+    }?.let { bitmap ->
+        GetCircleBitmap.make(bitmap)
+    }
+}
+
+
+@OptIn(ExperimentalCoroutinesApi::class)
+suspend fun Context.loadImageFromUrl(path: String): Bitmap =
+    suspendCancellableCoroutine { continuation ->
+        Glide
+        .with(this)
+        .asBitmap()
+        .timeout(6000000)
+        .load(path)
+        .into(object : CustomTarget<Bitmap>() {
+            override fun onResourceReady(resource: Bitmap, x: Transition<in Bitmap>?) {
+                continuation.resume(resource){}
+            }
+
+            override fun onLoadCleared(placeholder: Drawable?) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+
+
+data class PostNotification(
     val recipientId: Long,
     val threadName: String,
     val threadImage: String,
     val authorName: String,
+    val authorImage: String,
     val postContent: String,
     val published: Long,
     val threadType: Byte,
